098ad45f9da9f4242c8d463b8c50f9752809a535
Ryan Sutrisno <ryan.sutrisno@gmail.com>
2020-02-27 14:23:58 +0700

    desain tampilan notes

---
+++ src/components/FormNote.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="formNotes">
+        <div class="menu">
+            <button type="button" class="bg-danger btn btn-delete">Delete</button>
+            <button type="submit" class="bg-success btn">Save</button>
+        </div>
+        <div class="content">
+            <input type="text" class="text" placeholder="Title">
+            <textarea class="text textarea" placeholder="Tuliskan catatan kamu disini..."></textarea>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'formNotes',
+    data: function() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+.menu{
+     background: #f7f7f7;
+     padding:10px 25px;
+     margin-bottom: 25px;
+     text-align:right;
+     border-bottom: 1px solid #e8e6e6;
+}
+.btn-delete{ margin-right:10px; }
+.content{
+     padding: 0px 25px;
+}
+.text{
+     display: block;
+     width: 100%;
+     padding: 0px;
+     font-size: 20px;
+     font-weight: bold;
+     color: #2c3e50;
+     border: none;
+     margin-bottom: 10px;
+     box-sizing: border-box;
+     outline: none;
+}
+.textarea{
+     min-height: 350px;
+     font-size: 15px;
+     font-weight: lighter;
+     line-height: 30px;
+}
+.loader{
+     vertical-align: middle;
+}
+</style>
---
+++ src/components/ListNote.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="listNotes">
+        <ul>
+            <li v-for="(row, index) in notes" :key="index">
+                <button class="btn-note">
+                    <label>{{ row.title }}</label>
+                    <span>{{ row.description }}</span>
+                </button>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'listNotes',
+    data: function() {
+        return {
+            notes: [
+                { title: 'Catatan',
+                description: 'Ini isi catatan'}
+            ]
+        }
+    }
+}
+</script>
+
+<style scoped>
+ul {
+     list-style-type: none;
+     padding: 0;
+     margin:0px;
+}
+.btn-note {
+     text-align: left;
+     border: none;
+     border-bottom: 1px solid gainsboro;
+     padding: 0px 15px;
+     cursor: pointer;
+     outline: none;
+     background: #f7f7f7;
+     height: 150px;
+     width: 100%;
+}
+.btn-note:hover {
+     background:#eaeaea;
+}
+.btn-note label{
+     display:block;
+     color: #444444;
+     font-size: 1.5em;
+     margin-bottom: 15px;
+}
+.btn-note span{
+    color: #545454;
+}
+</style>
